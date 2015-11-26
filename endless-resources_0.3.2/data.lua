7c95419c8444186051a727c9ed59496c652fc494
gorre9090 <gorre9090@gmail.com>
2015-11-26 09:47:49 -0500

    Endless Resources v0.3.2
    Updated

--- endless-resources_0.3.2/data.lua
+++ endless-resources_0.3.2/data.lua
@@ -1,5 +1,5 @@
 for k, v in pairs(data.raw.resource) do
-   if not data.raw.resouces[k].infinite then
+   if not data.raw.resouce[k].infinite then
         data.raw.resource[k].infinite = true
         data.raw.resource[k].minimum  = 175
         data.raw.resource[k].normal   = 350
