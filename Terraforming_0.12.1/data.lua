73fcfb0decfce0d1cbe8fb620c7406018381f89c
gorre9090 <gorre9090@gmail.com>
2015-11-28 11:26:37 -0500

    Update v3

--- Terraforming_0.12.1/data.lua
+++
@@ -1,6 +0,0 @@
-require ("prototypes.technology")
-require ("prototypes.recipe-category")
-require ("prototypes.recipe")
-require ("prototypes.item")
-require ("prototypes.drawbridge")
-require ("prototypes.entity")
